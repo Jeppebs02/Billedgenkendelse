c96abe524391602e25be9ee5771621aeae4b6b43
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-10-10 09:10:16 +0200

    Add new image and model, update main.py to use them
    Added 'man_with_hat.jpg' to the images directory and 'best.pt' model to the models directory. Updated main.py to use 'man_with_hat.jpg' as the input image.

--- Python-code/Billedegenkendelse/main.py
+++ Python-code/Billedegenkendelse/main.py
@@ -30,7 +30,8 @@ if __name__ == '__main__':
 
     # Configuration
     #IMAGE_FILE_NAME = "woman.jpg"
-    IMAGE_FILE_NAME = "closedeyes.jpg"
+    #IMAGE_FILE_NAME = "closedeyes.jpg"
+    IMAGE_FILE_NAME = "man_with_hat.jpg"
 
     IMAGE_FILE = os.path.join("images", IMAGE_FILE_NAME)
 
