a3a926e432e53c4abbb0141d6c574342534b8b61
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-10-27 08:56:00 +0100

    Add Ionic Vue integration to project
    Installed @ionic/vue and @ionic/vue-router dependencies and updated main.js to initialize IonicVue. The app now uses Ionic Vue for enhanced UI components and navigation, and waits for the router to be ready before mounting.

--- App/KoerekortApp/src/main.js
+++ App/KoerekortApp/src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { IonicVue } from '@ionic/vue';
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(IonicVue)
 app.use(router)
 
-app.mount('#app')
+router.isReady().then(() => {
+  app.mount('#app');
+});
