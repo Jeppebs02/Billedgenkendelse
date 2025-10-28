67c830d03fa322623037d2ed80b17430bc057059
MatiasHolmNielsen <10519141@ucn.dk>
2025-10-28 11:09:17 +0100

    Display image from data store in ResultComponent
    Imports necessary Vue and Ionic modules, retrieves the image source from the data store, and displays it in the ResultComponent if available.

--- App/KoerekortApp/src/components/ResultComponent.vue
+++ App/KoerekortApp/src/components/ResultComponent.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { IonButton, useIonRouter } from '@ionic/vue';
+import { useDataStore } from '@/stores/dataStore.js'
 
-<template></template>
+const httpMsg = ref('')
+const dataStore = useDataStore()
+const imageSrc = ref(null)
 
-<style scoped></style>
+imageSrc.value = dataStore.getImageSrc()
+</script>
+
+<template>
+  <div style="display:grid; gap:12px; max-width: 420px;">
+
+    <img
+      v-if="imageSrc"
+      :src="imageSrc"
+      alt="snapshot"
+      style="max-width: 100%; border: 1px solid #ddd; border-radius: 8px;"
+    />
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
