5eb196f31a35f994de96591a22d90026d5ab9c35
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-11-05 13:08:08 +0100

    Rename Face_direction to face_direction and update imports
    Renamed the Face_direction module to face_direction for consistency with Python naming conventions. Updated all relevant imports and references in logic_controller.py to reflect the new module name and variable casing.

--- Python-code/Billedegenkendelse/logic/logic_controller.py
+++ Python-code/Billedegenkendelse/logic/logic_controller.py
@@ -6,7 +6,7 @@ from .is_image_clear.pixelation_detector import PixelationDetector
 from .types import AnalysisReport, CheckResult, Requirement, Severity
 from mediapipe.tasks.python import vision
 from typing import Tuple, Union, Optional, List
-from logic.Face_direction.Face_looking_at_camera import FaceLookingAtCamera
+from logic.face_direction.Face_looking_at_camera import FaceLookingAtCamera
 import math
 
 
@@ -19,7 +19,7 @@ class LogicController:
         self.exposure_check = exposure_check()
         # Here we can set a float which is the threshold for pixelation detection
         self.pixelation_detector = PixelationDetector()
-        self.FaceLookingAtCamera = FaceLookingAtCamera()
+        self.faceLookingAtCamera = FaceLookingAtCamera()
 
 
 
@@ -98,7 +98,7 @@ class LogicController:
         checks.append(self.exposure_check.analyze(image_bytes, face_landmarker_result))
 
         # 10) face looking straight
-        checks.append(self.Face_looking_at_camera.FaceLookingAtCamera(landmarks=face_landmarker_result))
+        checks.append(self.face_looking_at_camera.FaceLookingAtCamera(landmarks=face_landmarker_result))
 
 
         overall_pass = all(c.passed for c in checks)
