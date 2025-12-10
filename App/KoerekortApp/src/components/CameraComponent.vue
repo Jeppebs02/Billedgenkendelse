99938de027e2e6e47366aa2f356f91b3b7a4c3fb
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-12-10 10:50:38 +0100

    Update CameraComponent.vue

--- App/KoerekortApp/src/components/CameraComponent.vue
+++ App/KoerekortApp/src/components/CameraComponent.vue
@@ -61,7 +61,7 @@ async function addPhoto() {
   try {
     loading.value = true
     // DO NOT set Content-Type manually. The browser sets it for FormData.
-    const resp = await fetch('http://127.0.0.1:5001/analyze', {
+    const resp = await fetch('http://api.terragrouplabs.net/analyze', {
       method: 'POST',
       headers: {
         'x-api-key': 'a541fe33-6c48-490c-b71a-eadab16594de'
