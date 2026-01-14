2af3067696deaea0ef4887826d667cf6249a996e
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2026-01-14 10:45:14 +0100

    Update main.py

--- Python-code/Billedegenkendelse/main.py
+++ Python-code/Billedegenkendelse/main.py
@@ -4,11 +4,6 @@ import cv2
 import os
 import numpy as np
 
-
-
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 from logic.logic_controller import LogicController
 
 
